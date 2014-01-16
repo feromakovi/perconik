4efc181b638dc620698e9df8dbd4528883a0b098
Richard Samela <feromakovi@gmail.com>
2014-01-16 18:36:31 +0100

    Refactoring - changed input parameters type to final, implement method for tokenise source code from file and from string

--- sk.stuba.fiit.ProgrammerProportion/src/sk/stuba/fiit/programmerproportion/utils/SourceCode.java
+++ sk.stuba.fiit.ProgrammerProportion/src/sk/stuba/fiit/programmerproportion/utils/SourceCode.java
@@ -1,15 +1,18 @@
 package sk.stuba.fiit.programmerproportion.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 
 public final class SourceCode {
 
-	public static final List<String> parseLines(String block){
+	public static final List<String> parseLines(final String block){
 		List<String> validLines = new ArrayList<String>();
 		if(block != null){
 			String[] lines = block.split("\n");
@@ -21,20 +24,34 @@ public final class SourceCode {
 		return validLines;
 	}
 	
-	public static final String normalizeCode(String line){
+	public static final String normalizeCode(final String line){
 		return line.replaceAll("\\s", "");
 	}
 	
-	private static final boolean isValidLineOfCode(String line){
+	private static final boolean isValidLineOfCode(final String line){
 		boolean valid = true;
 		valid &= line.replaceAll("\\s", "").length() > 1;
 		return valid;
 	}
 	
+	public static final String[] tokenize(final File sourceFile){
+		String[] tokens = new String[0];
+		try {
+			tokens = tokenize(Files.toString(sourceFile, Charsets.UTF_8));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return tokens;
+	}
+	
+	public static final String[] tokenize(final String sourceCode){
+		return sourceCode.replaceAll("[(){}\n\",._':;?&!=@+/*<>]", " ").replaceAll("\\s+", " ").split("\\s");
+	}
+	
 	public static void main(String... args) throws IOException{
 		String code = Files.toString(Paths.get("/Users/feromakovi/Dropbox/WorkspaceAndroid/balbies/src/com/balbies/android/RegistrationActivity.java").toFile(), Charset.defaultCharset());
 		
-		for(String l : parseLines(code))
-			System.out.println(l);
+		for(String l : tokenize(code))
+			System.out.print(l + " ");
 	}
 }
