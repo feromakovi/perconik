39cc1d3f50a69773ef361a62dcc5d3e3ce873c33
Richard Samela <feromakovi@gmail.com>
2014-04-07 19:46:27 +0200

    Updated tokenising of source code

--- sk.stuba.fiit.ProgrammerProportion/src/sk/stuba/fiit/programmerproportion/utils/SourceCode.java
+++ sk.stuba.fiit.ProgrammerProportion/src/sk/stuba/fiit/programmerproportion/utils/SourceCode.java
@@ -7,6 +7,9 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import jgibblda.Inferencer;
 import jgibblda.LDACmdOption;
@@ -20,6 +23,7 @@ import com.google.common.io.Files;
 public final class SourceCode {
 	
 	private static final String SEPARATORS = "~^$&|?\\_,-.:\"'(){}[]=<>;%@+/*#!1234567890";
+	static Pattern mPackagePattern = Pattern.compile("package (.*?);");
 
 	public static final List<String> parseLines(final String block){
 		List<String> validLines = new ArrayList<String>();
@@ -53,6 +57,62 @@ public final class SourceCode {
 		return tokens;
 	}
 	
+	public static final String extractPackage(final File sourceFile){
+		String extractedPackage = null;
+		try {
+			extractedPackage = extractPackage(Files.toString(sourceFile, Charsets.UTF_8));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return extractedPackage;
+	}
+	
+	public static final String extractPackage(final String text){
+		Matcher matcher = mPackagePattern.matcher(text);
+		if(matcher.find())
+			return matcher.group(1);
+		return null;
+	}
+	
+	public static final String[] tokenize(final String sourceCode){ //sourceCode.replaceAll("[(){}\n\",._':;?&!=@+/*<>]", " ").replaceAll("\\s+", " ").split("\\s");
+		IdentifierNameTokeniserFactory factory = new IdentifierNameTokeniserFactory();
+		factory.setSeparatorCharacters(SEPARATORS);
+		IdentifierNameTokeniser tokeniser = factory.create();
+		String[] tokens = tokeniser.tokenise(sourceCode);
+		String tokenized = removeSet(representationOf(" ", tokens).replaceAll("\n", " "), StopWords.ENGLISH, true); 
+		tokenized = tokenized.replaceAll("\\b[^\\s]\\b", ""); //remove all one character words
+		tokenized = tokenized.replaceAll("\\b.{2}\\b", ""); //remove all two characters words
+		tokenized = tokenized.replaceAll("\\s+", " ");
+		if(tokenized.startsWith(" "))
+			tokenized = tokenized.replaceFirst(" ", "");		
+		return tokenized.split(" ");
+	}
+	
+	public static final String removeSeparators(final String code){
+        return code.replaceAll("[~^$&|?\\_,-\\.:\"'(){}\\[\\]=<>;%@+/*#!]", " ");
+	}
+	
+	public static final String removeSet(String code, Set<String> words, boolean ignoreCase){
+		if(ignoreCase)
+			code = code.toLowerCase();
+		Iterator<String> iterator = words.iterator();
+		while(iterator.hasNext()){
+			String replacement = (ignoreCase) ? iterator.next().toLowerCase() : iterator.next();
+			code = code.replaceAll("\\b" + replacement + "\\b", "");
+		}			
+		return code.replaceAll("\\s+", " ");
+	}
+	
+	public static final String representationOf(final String delimiter, final String... strings){
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < strings.length; i++){
+			builder.append(strings[i]);
+			if(i < (strings.length -1))
+				builder.append(delimiter);
+		}
+		return builder.toString();
+	}
+	
 	public static final Model inference(final String[] words){
 		LDACmdOption ldaOption = new LDACmdOption(); 
 		ldaOption.inf = true; 
@@ -65,25 +125,6 @@ public final class SourceCode {
 		return newModel;
 	}
 	
-	public static final String[] tokenize(final String sourceCode){ //sourceCode.replaceAll("[(){}\n\",._':;?&!=@+/*<>]", " ").replaceAll("\\s+", " ").split("\\s");
-		IdentifierNameTokeniserFactory factory = new IdentifierNameTokeniserFactory();
-		factory.setSeparatorCharacters(SEPARATORS);
-		IdentifierNameTokeniser tokeniser = factory.create();
-		String[] tokens = tokeniser.tokenise(sourceCode);
-		StringBuilder builder = new StringBuilder();
-		for(String t : tokens)
-			builder.append(t + " ");		
-		String tokenized = builder.toString().replaceAll("\n", " ").toLowerCase();
-		Iterator<String> iterator = StopWords.ALL.iterator();
-		while(iterator.hasNext())
-			tokenized = tokenized.replaceAll("\\b" + iterator.next().toLowerCase() + "\\b", "");
-		tokenized = tokenized.replaceAll("\\b[^\\s]\\b", "");
-		tokenized = tokenized.replaceAll("\\b.{2}\\b", "");
-		tokenized = tokenized.replaceAll("\\s+", " ");
-		if(tokenized.startsWith(" "))
-			tokenized = tokenized.replaceFirst(" ", "");		
-		return tokenized.split(" ");
-	}
 	
 	public static void main(String... args) throws IOException{
 		String code = Files.toString(Paths.get("/Users/feromakovi/Desktop/token.j").toFile(), Charset.defaultCharset());
