b43031a6aeb87a1cfc00157e0ff29d30b7b93c91
Richard Samela <feromakovi@gmail.com>
2014-04-08 02:06:53 +0200

    Created ASTVisitor class for parsing Java classes and tokenising text, then do inference

---
+++ sk.stuba.fiit.ProgrammerProportion/src/sk/stuba/fiit/programmerproportion/handlers/ClassVisitor.java
@@ -0,0 +1,35 @@
+package sk.stuba.fiit.programmerproportion.handlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+import sk.stuba.fiit.programmerproportion.utils.LDAHelper;
+import sk.stuba.fiit.programmerproportion.utils.SourceCode;
+import sk.stuba.fiit.programmerproportion.utils.StopWords;
+
+public class ClassVisitor extends ASTVisitor{
+	
+	private List<String> mTokens = new ArrayList<String>();
+
+	@Override
+	public boolean visit(TypeDeclaration node) {
+		try{
+			final String originalCode = node.toString();
+			final String noJavaCode = SourceCode.removeSet(SourceCode.removeSeparators(originalCode), StopWords.JAVA, false); //Not ignore case because of INTT - it won't separate words 
+			String[] tokens = SourceCode.tokenize(noJavaCode);
+			if(tokens != null){
+				tokens = SourceCode.removeSet(SourceCode.representationOf(" ", tokens), StopWords.OFTEN, true).split(" ");
+				for(String token : tokens)
+					mTokens.add(token);
+			}
+		}catch(Exception e){}
+		return super.visit(node);
+	}
+	
+	public void inference(){
+		LDAHelper.inference(mTokens.toArray(new String[mTokens.size()]));
+	}
+}
